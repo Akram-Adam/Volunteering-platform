4c460e433ade345bd525306109f33a1d663ae1b8
Akram-Adam <akramadam050@gmail.com>
2025-01-09 23:09:22 +0000

    posted vlounteer  done

--- Template-vue/src/components/VolunteerComponents/PostedOpportunitiesList.vue
+++ Template-vue/src/components/VolunteerComponents/PostedOpportunitiesList.vue
@@ -89,7 +89,7 @@
 </template>
 
 <script>
-import { useOpportunitiesStore } from "@/stores/useOpportunitiesStore";
+import { useOpportunitiesStore } from "@/stores/useOpportunitiesStore ";
 
 export default {
   name: "PostedOpportunitiesList",
